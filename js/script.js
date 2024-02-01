a4393b48d59a80b449c948ff7e02222f42fcd7d8
Justice Ampofo <justiceampofo@gmail.com>
2024-02-01 18:35:24 -0500

    bug fix for the toCapitalized function

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ function toUpper(value) {
 function toCapitalized(value) {
 	// Split the text into sentences based on '.', '!', or '?' followed by a space or end of string.
 	const sentenceEndings = /([.!?]\s|\s$)/;
-	const sentences = value.split(sentenceEndings);
+	const sentences = value.toLowerCase().split(sentenceEndings);
 
 	// Process each sentence to capitalize the first letter of each word.
 	const capitalizedSentences = sentences.map((part) => {
