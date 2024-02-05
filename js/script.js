8d88dbe1942815b31ee98d5176f489679f442908
Justice Ampofo <justiceampofo@gmail.com>
2024-02-05 18:27:09 -0500

    add title case function

--- js/script.js
+++ js/script.js
@@ -24,6 +24,10 @@ function displayButtons() {
 			name: 'Capitalized Case',
 		},
 		{
+			className: 'title',
+			name: 'Title Case',
+		},
+		{
 			className: 'download',
 			name: 'Download Text',
 		},
@@ -62,6 +66,8 @@ function onTextSubmit(e) {
 		formTextArea.value = toUpper(formTextArea.value);
 	} else if (targetClassName.includes('capitalized')) {
 		formTextArea.value = toCapitalized(formTextArea.value);
+	} else if (targetClassName.includes('title')) {
+		formTextArea.value = toTitle(formTextArea.value);
 	}
 }
 
@@ -99,6 +105,33 @@ function toCapitalized(value) {
 
 	// Join the parts back together into the final string.
 	return capitalizedSentences.join('');
+}
+
+function toTitle(value) {
+	const exceptions = ['of', 'an', 'and', 'the', 'or'];
+	// This regex will match either sentence endings followed by space (or end of string) or words directly.
+	const sentenceEndingsOrWords = /([.!?]\s|\s$)|\w+('\w+)?/g;
+
+	// Process the text, identifying sentence parts and words.
+	let isStartOfSentence = true; // Flag to indicate the start of a new sentence or the whole text.
+
+	return value.toLowerCase().replace(sentenceEndingsOrWords, (match) => {
+		// If the match is a sentence ending, keep it as is and mark the start of a new sentence.
+		if (/[.!?]\s|\s$/.test(match)) {
+			isStartOfSentence = true;
+			return match;
+		}
+
+		// For words, capitalize if it's the start of a sentence or not an exception.
+		// Lowercase exceptions unless it's the start of a sentence.
+		if (isStartOfSentence || !exceptions.includes(match)) {
+			isStartOfSentence = false; // Any word encountered marks the end of a start of a sentence.
+			return match.charAt(0).toUpperCase() + match.slice(1);
+		} else {
+			isStartOfSentence = false; // Ensure to reset for words that are exceptions but not at the start.
+			return match; // Return exceptions in lowercase.
+		}
+	});
 }
 
 function init() {
