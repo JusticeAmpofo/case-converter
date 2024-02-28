21888e1873aa881ebf69687fcd080e81d6d12aa2
Justice Ampofo <justiceampofo@gmail.com>
2024-02-28 18:00:01 -0500

    add tests for the toCapitalized function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import toSentence from './functions/toSentence/toSentence';
 import toLower from './functions/toLower/toLower';
 import toUpper from './functions/toUpper/toUpper';
+import toCapitalized from './functions/toCapitalized/toCapitalized';
 
 import './css/style.css';
 
@@ -108,27 +109,6 @@ function onTextSubmit(e) {
 		default:
 			break;
 	}
-}
-
-function toCapitalized(value) {
-	// Split the text into sentences based on '.', '!', or '?' followed by a space or end of string.
-	const sentenceEndings = /([.!?]\s|\s$)/;
-	const sentences = value.toLowerCase().split(sentenceEndings);
-
-	// Process each sentence to capitalize the first letter of each word.
-	const capitalizedSentences = sentences.map((part) => {
-		// Check if the part is a sentence ending or whitespace, return as is if true.
-		if (sentenceEndings.test(part)) return part;
-
-		// Split the sentence into words, capitalize the first letter of each word, then join back.
-		return part
-			.split(' ')
-			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-			.join(' ');
-	});
-
-	// Join the parts back together into the final string.
-	return capitalizedSentences.join('');
 }
 
 function toTitle(value) {
